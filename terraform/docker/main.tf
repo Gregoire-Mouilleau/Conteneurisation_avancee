192db3a9bc2b60908201c00773b09e30fb9d0338
Gregoire Mouilleau <gregoire.mouilleau@gmail.com>
2026-05-28 08:57:37 +0200

    fix: MYSQL_USER in docker-compose, OCI terraform docker+k8s

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -75,19 +75,28 @@ resource "oci_core_security_list" "docker_sl" {
   ingress_security_rules {
     protocol = "6"
     source   = "0.0.0.0/0"
-    tcp_options { min = 22;  max = 22  }
+    tcp_options {
+      min = 22
+      max = 22
+    }
   }
 
   ingress_security_rules {
     protocol = "6"
     source   = "0.0.0.0/0"
-    tcp_options { min = 80;  max = 80  }
+    tcp_options {
+      min = 80
+      max = 80
+    }
   }
 
   ingress_security_rules {
     protocol = "6"
     source   = "0.0.0.0/0"
-    tcp_options { min = 443; max = 443 }
+    tcp_options {
+      min = 443
+      max = 443
+    }
   }
 }
 
